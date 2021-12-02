bc6ffa0afaae005835f6a87fca765fa35e88cb37
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2021-12-02 13:44:14 +0000

    Added Player and PlayerMovement

--- NAR Unity Project/Assets/Scripts/PlayerMovement.cs
+++ NAR Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController characterController;
+
+    [SerializeField] private float moveSpeed;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Applies gravity
+        characterController.Move(-transform.up * 9.81f * Time.deltaTime);
+
+        if (Input.GetKey("up"))
+        {
+            characterController.Move(transform.forward * moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey("down"))
+        {
+            characterController.Move(-transform.forward * moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey("left"))
+        {
+            characterController.Move(-transform.right * moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey("right"))
+        {
+            characterController.Move(transform.right * moveSpeed * Time.deltaTime); 
+        } 
     }
 }
