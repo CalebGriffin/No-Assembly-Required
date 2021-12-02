a40994e46f2cd7e6f30e3abcd135dfeb268f6188
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2021-12-02 15:09:42 +0000

    Menu Continued
    + Setup for Menus Moving
    (Currently Not Fully Functioning)

--- NAR Unity Project/Assets/Menu Folder/UIMEnuBasics.cs
+++ NAR Unity Project/Assets/Menu Folder/UIMEnuBasics.cs
@@ -4,29 +4,107 @@ using UnityEngine;
 
 public class UIMEnuBasics : MonoBehaviour
 {
-    public int currentMenu = 1;
-    public GameObject[] Menus;
-    public int desiredMenu;
+    [SerializeField]
+    int currentMenu = 1;
+    [SerializeField]
+    GameObject[] Menus;
+    [SerializeField]
+    int desiredMenu = 1;
     public Transform removedLocation;
     public Transform presentLocation;
     public Transform transferLocation;
+    float speed = 1f;
+    float step;
+    bool notInMotion;
 
     void Start()
     {
         Menus = GameObject.FindGameObjectsWithTag("Menu");
         Invoke("MenuCheck", 0.5f);
+        
+    }
+    void Update()
+    {
+        step = speed * Time.deltaTime;
     }
 
     void MenuCheck()
     {
-        if (currentMenu != desiredMenu)
+        Debug.Log("MenuCheck Called");
+        if (notInMotion == false)
         {
-            //if(Menus(currentMenu).transform != RemovedLocation || Menus(currentMenu).transform != transferLocation)
+            for (int x = 0; x == Menus.Length; x++)
             {
-
+                Debug.Log("x is equal to" + x);
+                if (Menus[x].transform == presentLocation)
+                {
+                    currentMenu = x + 1;
+                }
             }
-            
         }
+        Invoke("MenuMove", 0.5f);
+        
+    }
+
+    void MenuMove()
+    {
+        StartCoroutine(OffMove());
+        if (currentMenu == desiredMenu)
+        {
+            notInMotion = true;
+        }
+        
     }
     
+    public void ChangeToMenu1()
+    {
+        desiredMenu = 1;
+    }
+    public void ChangeToMenu2()
+    {
+        desiredMenu = 2;
+    }
+    public void ChangeToMenu3()
+    {
+        desiredMenu = 3;
+    }
+
+    IEnumerator OffMove()
+    {
+        while (Menus[currentMenu].transform != removedLocation)
+        {
+            notInMotion = false;
+            if (Menus[currentMenu].transform == presentLocation)
+            {
+                Menus[currentMenu].transform.position = Vector3.MoveTowards(presentLocation.position, transferLocation.position, speed);
+            }
+            else if (Menus[currentMenu].transform == transferLocation)
+            {
+                Menus[currentMenu].transform.position = Vector3.MoveTowards(transferLocation.position, removedLocation.position, speed);
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+        StartCoroutine(MoveOn());
+        yield return new WaitForSeconds(0.1f);
+        Invoke("MenuCheck", 0.5f);
+    }
+    IEnumerator MoveOn()
+    {
+        notInMotion = false;
+        while (Menus[desiredMenu].transform != presentLocation)
+        {
+            if (Menus[currentMenu].transform != presentLocation || Menus[currentMenu] != transferLocation)
+            {
+                Menus[currentMenu].transform.position = Vector3.MoveTowards(removedLocation.position, transferLocation.position, speed);
+            }
+            else if (Menus[currentMenu].transform == transferLocation)
+            {
+                Menus[currentMenu].transform.position = Vector3.MoveTowards(transferLocation.position, presentLocation.position, speed);
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(0.1f);
+        Invoke("MenuCheck", 0.5f);
+    }
 }
