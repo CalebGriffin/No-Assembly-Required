1b2a0da2a1390f55100be75ff802a32a74222c1a
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2021-12-08 11:14:58 +0000

    Menu Update
    + Added Loading of Levels
    + Added Level Buttons

--- NAR Unity Project/Assets/Menu Folder/LevelSelect.cs
+++ NAR Unity Project/Assets/Menu Folder/LevelSelect.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int Number;
+    string NeededScene;
+    public GameObject LoadingScreen;
+    
+    
+
+    public void Button1Activate()
     {
-        
+        Number = 1;
+        StartCoroutine("LoadLevel");
+    }
+    public void Button2Activate()
+    {
+        Number = 2;
+        StartCoroutine("LoadLevel");
+    }
+    public void Button3Activate()
+    {
+        Number = 3;
+        StartCoroutine("LoadLevel");
+    }
+    public void Button4Activate()
+    {
+        Number = 4;
+        StartCoroutine("LoadLevel");
+    }
+    IEnumerator LoadLevel()
+    {
+        NeededScene = "Level" + Number;
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(NeededScene);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            LoadingScreen.SetActive(true);
+        }
+        if (asyncLoad.isDone)
+        {
+            LoadingScreen.SetActive(false);
+            SceneManager.UnloadSceneAsync("MainMenu");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
